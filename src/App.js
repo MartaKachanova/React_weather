b797b0c9090d369ef936f4c48142a7c3ccfc9aed
Marta_kachanova <marta.kachanova@gmail.com>
2022-10-24 01:35:40 +0300

    WEATHER

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 const api = {
   key: 'c7616da4b68205c2f3ae73df2c31d177',
@@ -7,50 +7,52 @@ const api = {
 }
 
 function App() {
-  const [city, setCity] = useState('');
-  const [weather, setWeather] = useState('');
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState("");
 
-  const search = evt => {
-    if (evt.key === 'Enter') {
-      fetch(`${api.base}weather?q=${city}&units=metric&appid=${api.key}`) 
-        .then(res => res.json())
-        .then(result => {         
+  const search = (evt) => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${city}&units=metric&appid=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
           setWeather(result);
-          setCity('');
+          setCity("");
         });
     }
-  }
+  };
 
   return (
-    <div className={(typeof weather.main != 'undefined')}>
+    <div className={typeof weather.main != "undefined"}>
       <main>
-      <div className='title'>ПРОГНОЗ ПОГОДИ</div>
-        <div className='search-box'>
+        <div className="title">ПРОГНОЗ ПОГОДИ</div>
+        <div className="search-box">
           <input
-            type='text'
-            className='search-bar'
-            placeholder='Введіть місто...'
-            onChange={e => setCity(e.target.value)}
+            type="text"
+            className="search-bar"
+            placeholder="Введіть місто..."
+            onChange={(e) => setCity(e.target.value)}
             value={city}
             onKeyPress={search}
           />
         </div>
-        {(typeof weather.main != 'undefined') ? (
-        <div>
-          <div className='location-box'>
-            <div className='location'>{weather.name}</div>
-          </div>
-          <div className='weather-box'>
-            <div className='temp'>
-              {Math.round(weather.main.temp)}°c
+        {typeof weather.main != "undefined" ? (
+          <div>
+            <div className="location-box">
+              <div className="location">{weather.name}</div>
             </div>
-            <div className='weather'>{weather.weather[0].main}</div>
+            <div className="weather-box">
+              <div className="temp">{Math.round(weather.main.temp)}°c</div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
           </div>
-        </div>
-        ) : ('')}
+        ) : (
+          ""
+        )}
       </main>
     </div>
   );
 }
+
+
 
 export default App;
