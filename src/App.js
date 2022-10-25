deb980937867c27c3d6b8195e1088200942af712
Marta_kachanova <marta.kachanova@gmail.com>
2022-10-25 14:44:13 +0300

    react with router

--- src/App.js
+++ src/App.js
@@ -1,55 +1,24 @@
-import React, { useState } from "react";
 import "./App.css";
-
-const api = {
-  key: '6f0cdf56c54d4fddba1893c317657fc9',
-  base: 'https://api.openweathermap.org/data/2.5/'
-}
+import { Routes, Route } from "react-router-dom";
+import About from "./components/About";
+import List from "./components/List";
+import Layout from "./components/Layout";
 
 function App() {
-  const [city, setCity] = useState("");
-  const [weather, setWeather] = useState("");
-
-  const search = (evt) => {
-    if (evt.key === "Enter") {
-      fetch(`${api.base}weather?q=${city}&units=metric&appid=${api.key}`)
-        .then((res) => res.json())
-        .then((result) => {
-          setWeather(result);
-          setCity("");
-        });
-    }
-  };
-
   return (
-    <div className={typeof weather.main != "undefined"}>
-      <main>
-        <div className="title">ПРОГНОЗ ПОГОДИ</div>
-        <div className="search-box">
-          <input
-            type="text"
-            className="search-bar"
-            placeholder="Введіть місто..."
-            onChange={(e) => setCity(e.target.value)}
-            value={city}
-            onKeyPress={search}
-          />
-        </div>
-        {typeof weather.main != "undefined" ? (
-          <div>
-            <div className="location-box">
-              <div className="location">{weather.name}</div>
-            </div>
-            <div className="weather-box">
-              <div className="temp">{Math.round(weather.main.temp)}°c</div>
-              <div className="weather">{weather.weather[0].main}</div>
-            </div>
-          </div>
-        ) : (
-          ""
-        )}
-      </main>
-    </div>
+    <>
+      <div className="wrapper">
+        <header>
+          <div className="title">ПРОГНОЗ ПОГОДИ</div>
+        </header>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="about" element={<About />} />
+            <Route index element={<List />} />
+          </Route>
+        </Routes>
+      </div>
+    </>
   );
 }
 
